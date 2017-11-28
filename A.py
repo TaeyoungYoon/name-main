5a53ac87a0f3e11a560efbeb59cd3aa363819d82
ysryu <ysryu@mju.ac.kr>
2017-11-28 22:10:09 +0900

    name-main exercise

---
+++ A.py
@@ -0,0 +1,9 @@
+def func() :
+	print("function in A.py")
+
+print("top level in A.py")
+
+if __name__ == "__main__" :
+	print("A.py is executing directly")
+else :
+	print("A.py is imported")
---
+++ B.py
@@ -0,0 +1,9 @@
+import A
+
+
+print("top level in B.py")
+
+if __name__ == "__main__" :
+	print("B.py is executing directly")
+else :
+	print("B.py is imported")
